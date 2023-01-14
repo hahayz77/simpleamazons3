749aff7730d96f01c0ce0ce33859f72bc3cfbd4c
hahayz77 <danilo__danilo@hotmail.com>
2023-01-14 08:19:27 -0300

    local upload without buffer

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
-const app = express();
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
 const port = 3000;
+
+const app = express();
 
 app.get('/', (req, res) => {
   res.json({response:'Hello World!'})
+});
+
+app.post('/post', upload.single('file'), (req,res)=>{
+    console.log(req.file);
+    res.send(req.body);
 });
 
 app.listen(port, () => {
